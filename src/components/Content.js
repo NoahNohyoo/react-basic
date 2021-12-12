01a497194b3027e69b53b3bf0c86afeb606b45c4
noahNoh <cananohyoo@gmail.com>
2021-12-11 20:51:19 -0800

    react git init

---
+++ src/components/Content.js
@@ -0,0 +1,23 @@
+import React, {Component } from 'react';
+
+class Content extends Component {
+    render() {
+        return (
+        <article>
+            <h2>{this.props.title}</h2>
+            {this.props.desc}
+        </article>
+        );
+    }
+}
+
+// function Content() {
+//     return (
+//         <article>
+//             <h2>{this.props.title}</h2>
+//             {this.props.desc}
+//         </article>
+//     );
+// };
+
+  export default Content;
