bcd6f2296dc55436027156c4e6e3fcfede214b63
noahNoh <cananohyoo@gmail.com>
2021-12-15 17:18:11 -0800

    add shouldComponentUpdate in TOC

--- src/components/TOC.js
+++ src/components/TOC.js
@@ -1,34 +1,39 @@
 import React, {Component } from 'react';
 
 class TOC extends Component {
-    render() {
-      let lists = [];
-      const data = this.props.data;
-      let i = 0;
+  shouldComponentUpdate(newProps, newState) {
+    if (this.props.data === newProps.data) return false;
+    else return true;
+  };
 
-      while (i < data.length) {
-        lists.push(
-          <li key={data[i].id}>
-            <a 
-              href={"/content/"+data[i].id}
-              data-id={data[i].id}
-              onClick={function(e){
-                e.preventDefault();
-                this.props.onChangePage(e.target.dataset.id);
-              }.bind(this)}
-            >{data[i].title}</a>
-          </li>);
-        i++;
-      }
+  render() {
+    let lists = [];
+    const data = this.props.data;
+    let i = 0;
 
-      return (
-        <nav>
-          <ul>
-            {lists}
-          </ul>
-        </nav>
-      );
+    while (i < data.length) {
+      lists.push(
+        <li key={data[i].id}>
+          <a 
+            href={"/content/"+data[i].id}
+            data-id={data[i].id}
+            onClick={function(e){
+              e.preventDefault();
+              this.props.onChangePage(e.target.dataset.id);
+            }.bind(this)}
+          >{data[i].title}</a>
+        </li>);
+      i++;
     }
+
+    return (
+      <nav>
+        <ul>
+          {lists}
+        </ul>
+      </nav>
+    );
   }
+}
 
   export default TOC;
